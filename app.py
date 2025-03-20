91626473bbf154f87f95008af9b78c5cd389882e
AASHISH NIRANJAN BARATHYKANNAN <aashishniranjanb@gmail.com>
2025-03-20 18:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ import streamlit as st
 from utils.session_manager import initialize_session_state, get_llm_chain_from_session
 from components.sidebar import create_sidebar
 from components.chat_interface import display_chat_messages, handle_chat_input
+import google.generativeai as genai
+
+# Set your Google API key directly in the code
+GOOGLE_API_KEY = "AIzaSyBVOM4Pct30jaUcFUiXpbMy4hVOv2f3kKk"
+
+# Configure the Google Generative AI with the API key
+genai.configure(api_key=GOOGLE_API_KEY)
 
 # Set page title and logo
 st.set_page_config(
     page_title="Data Science Tutor",
-    page_icon="static/logo.png"  # Set the page icon
+    page_icon="static/ai_tutor_icon.png"  
 )
 
 # Initialize session state and LLM chain
