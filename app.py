61d04d71ae8338d2ac1656141eae9596027955a4
AASHISH NIRANJAN BARATHYKANNAN <aashishniranjanb@gmail.com>
2025-03-20 19:00:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ genai.configure(api_key=GOOGLE_API_KEY)
 # Set page title and logo
 st.set_page_config(
     page_title="Data Science Tutor",
-    page_icon="static/ai_tutor_icon.png"  
-)
+    page_icon="static/ai_tutor_icon.png" 
 
 # Initialize session state and LLM chain
 initialize_session_state()
